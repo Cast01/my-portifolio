83b397f830c8d580302ef3759ad6968f697f90c5
Cast01 <gabriel_floriano_castilho@outlook.com>
2023-06-16 23:55:16 -0300

    feat: Add new projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,9 +4,14 @@ import {ProjectsContainer} from './projectsStyle'
 import projectImage1 from '../../assets/project-img1.png'
 import projectImage2 from '../../assets/project-img2.png'
 import projectImage3 from '../../assets/project-img3.png'
+
 import nlw_i_eSport from '../../assets/nlw-i-eSport.png'
 import youtubeColne from '../../assets/youtube-clone.png'
 import nlwCopa from '../../assets/nlw-copa.png'
+import todo from '../../assets/todo.png'
+import ignite_feed from '../../assets/ignite-feed.png'
+import pokedex from '../../assets/pokedex.png'
+import ignite_timer from '../../assets/ignite-timer.png'
 
 // pegar a imagem dos projeto no guithub dela e usar para exemplos aqui.
 
@@ -18,12 +23,12 @@ export function Projects() {
             {name: 'NLW e-Sport', image: nlw_i_eSport, link: "https://nlw-ignite-e-sport-vercel.vercel.app/",},
             {name: 'Youtube Clone', image: youtubeColne, link: "https://youtube-clone-xi-opal.vercel.app/"},
             {name: 'NLW COPA', image: nlwCopa, link: "https://nlw-copa-next-js-responsive.vercel.app/"},
-            {name: '555', image: projectImage1, link: "https://nlw-ignite-e-sport-vercel.vercel.app/"},
-            {name: 'AAAAAA', image: projectImage1, link: "https://nlw-ignite-e-sport-vercel.vercel.app/"},
-            {name: 'abDSDSDSDa1', image: projectImage1, link: "https://nlw-ignite-e-sport-vercel.vercel.app/"},
+            {name: 'ToDo', image: todo, link: "https://vercel.com/cast01/rocketseat-ignite-react-js-desafio-todo-list"},
+            {name: 'Ignite Feed', image: ignite_feed, link: "https://ignite-feed-six-pi.vercel.app/"},
+            {name: 'Pokedex JS', image: pokedex, link: "https://pokedex-js-chi.vercel.app/"},
         ],
         [
-            {name: 'aba2', image: projectImage2, link: "https://nlw-ignite-e-sport-vercel.vercel.app/"},
+            {name: 'Ignite Timer', image: ignite_timer, link: "https://ignite-timer-five-peach.vercel.app/"},
             {name: 'aba2', image: projectImage2, link: "https://nlw-ignite-e-sport-vercel.vercel.app/"},
             {name: 'aba2', image: projectImage2, link: "https://nlw-ignite-e-sport-vercel.vercel.app/"},
             {name: 'aba2', image: projectImage2, link: "https://nlw-ignite-e-sport-vercel.vercel.app/"},
